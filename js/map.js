1fecf408e0c74fbcab5ab3001bf11013cea054bf
vinhpd <vinhpd@watermelon.vn>
2019-05-09 00:00:04 +0700

    update

--- js/map.js
+++ js/map.js
@@ -11,6 +11,35 @@ $(document).ready(function(){
     var listMarker = {};
     var listPopup = {};
 
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyA70nuqyypEmN7f_hw3cSJ6NhVtDxYxtzI",
+        authDomain: "meber-tracking.firebaseapp.com",
+        databaseURL: "https://meber-tracking.firebaseio.com",
+        projectId: "meber-tracking",
+        storageBucket: "meber-tracking.appspot.com",
+        messagingSenderId: "764503948585"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
+
+
+    $('body').on("click", ".btn-make-call", function () {
+       const username = $(this).attr('data-username');
+        const uniqueId = Math.random().toString(36).substring(2)
+            + (new Date()).getTime().toString(36);
+       const callId = "com_cmc_tracking_call_" + uniqueId;
+       const urlCall = "https://appr.tc/r/" + callId;
+       window.open(urlCall);
+
+       // notify client:
+        database.ref('online-users/' + username + '/' + 'calling-info').set({
+            "call-id": callId,
+            "status": "ringing"
+        });
+
+    });
+
     function addMarker(lat, long, userName, imageUrl){
         console.log("add marker long: " + long + ", lat: " + lat);
 
@@ -22,7 +51,7 @@ $(document).ready(function(){
             '<div id="marker" title="Marker">' +
             `<img class="img-avatar" src="${imageUrl}" alt="Avatar">` +
             `<span>${userName}</span>` +
-            `<a href="#"><img class="img-call" src="img/phone-call.png" ></a>` +
+            `<a href="#" class="btn-make-call" data-username="${userName}"><img class="img-call" src="img/phone-call.png" ></a>` +
             '</div>' +
             '</div>';
 
@@ -60,18 +89,6 @@ $(document).ready(function(){
             value.setMap(null);
         }
     }
-
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyA70nuqyypEmN7f_hw3cSJ6NhVtDxYxtzI",
-        authDomain: "meber-tracking.firebaseapp.com",
-        databaseURL: "https://meber-tracking.firebaseio.com",
-        projectId: "meber-tracking",
-        storageBucket: "meber-tracking.appspot.com",
-        messagingSenderId: "764503948585"
-    };
-    firebase.initializeApp(config);
-    var database = firebase.database();
 
 
     var meberUser = database.ref('online-users');
